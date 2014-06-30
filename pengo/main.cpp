7571aefd99149c0871d05fbf8a7b55d3be15bf74
Leonardo Korndorfer <leokorndorfer@gmail.com>
2014-06-30 17:53:46 -0300

    update

--- pengo/main.cpp
+++ pengo/main.cpp
@@ -409,9 +409,33 @@ void renderFloor() {
             glVertex3f(stage.getStagesizeX()-0.5,0.0,-0.5);
     glEnd();
 
+    // Create and bind a texture object
+    glGenTextures(1, &texture);
+	glBindTexture(type, texture);
+
+    // Set texture parameters
+	glTexParameteri(type, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(type, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(type, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(type, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+        // Create an RGBA image
+    rgba = (GLubyte *)malloc(info->bmiHeader.biWidth * info->bmiHeader.biHeight * 4);
+
+    i = info->bmiHeader.biWidth * info->bmiHeader.biHeight;
+    for( rgbaptr = rgba, ptr = bits;  i > 0; i--, rgbaptr += 4, ptr += 3)
+    {
+            rgbaptr[0] = ptr[2];     // windows BMP = BGR
+            rgbaptr[1] = ptr[1];
+            rgbaptr[2] = ptr[0];
+            rgbaptr[3] = (ptr[0] + ptr[1] + ptr[2]) / 3;
+    }
+
+    glTexImage2D(type, 0, 4, info->bmiHeader.biWidth, info->bmiHeader.biHeight,
+              0, GL_RGBA, GL_UNSIGNED_BYTE, rgba );
+
     glBegin(GL_QUADS);
         glNormal3f(0.0f,0.0f,1.0f);
-
         glVertex3f(-20.0, 0.0, 20.0);
         glVertex3f(-20.0, 3.0, 20.0);
         glVertex3f( 20.0, 3.0, 20.0);
